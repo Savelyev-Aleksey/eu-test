7f8576763fa370ec5b265702ec5d3ca3b3be7ae9
Aleksey Savelyev <saleksey92@gmail.com>
2017-05-06 12:25:50 +0600

    Refactor limit option. Added all select statement.

--- system/DB.php
+++ system/DB.php
@@ -1,7 +1,36 @@
 <?php
 
+/*
+ * The MIT License
+ *
+ * Copyright 2017 aleksey.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
 class DB
 {
+  /**
+   * Maximum affected records on select state
+   */
+  const QUERY_LIM = 1000;
+
   private static $mysqli = NULL;
 
 
@@ -142,7 +171,7 @@ class DB
     condition must set as array('filed={val-key}', array('{val-key}' => $value))
     set only unique key in query - see doc strtr() more info.
   */
-  static function select($table, $condition = NULL, $fields = '*', $order = NULL, $limit = 1000)
+  static function select($table, $condition = NULL, $fields = '*', $order = NULL, $limit = NULL)
   {
     self::init();
 
@@ -161,7 +190,7 @@ class DB
 
     // 5 safe limit
     if (!(is_int($limit) && $limit > 0))
-      $limit = 1000;
+      $limit = self::QUERY_LIM;
 
     $limit = 'LIMIT '. $limit;
 
