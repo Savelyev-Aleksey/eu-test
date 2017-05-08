e7b0c593a0d65ef4043b0b854d2c076ba96e1ba4
Aleksey Savelyev <saleksey92@gmail.com>
2017-05-08 14:21:40 +0600

    Rollback CSRF.

--- index.php
+++ index.php
@@ -22,13 +22,6 @@ spl_autoload_register(function ($class) {
 date_default_timezone_set('Asia/Novosibirsk');
 setlocale(LC_ALL, 'en_US.utf-8');
 
-
-// PHP RFC: Semi-Automatic CSRF Protection
-// [link]https://wiki.php.net/rfc/automatic_csrf_protection
-// Since PHP 7.1
-session_start(['csrf_rewrite'=>SESSION_CSRF_POST, 'csrf_validate'=>SESSION_CSRF_POST]);
-
-
 DB::init();
 
 Router::add_router('/', NULL, array('controller' => 'user', 'action' => 'index'));
