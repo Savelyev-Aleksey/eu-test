c186041ac72499cb2cff1d9e7b58f47497aa8895
Aleksey Savelyev <saleksey92@gmail.com>
2017-05-08 23:24:11 +0600

    Fix render buffer handling.

--- system/Render.php
+++ system/Render.php
@@ -30,58 +30,64 @@
  */
 class Render
 {
-    private static $counter = 0;
 
-    /**
-     * Use class name and $view variable such path to file
-     * @param string $view
-     * @throws Exception if view file not found
-     */
-    static public function view($view, $vars = [])
+  private static $counter = 0;
+
+  /**
+   * Use class name and $view variable such path to file
+   * @param string $view
+   * @throws Exception if view file not found
+   */
+  static public function view($view, $vars = [])
+  {
+    if (!self::$counter++)
     {
-        self::$counter++;
-        $model = strtolower(substr(get_called_class(), 11));
+      ob_start();
+    }
+    $model = strtolower(substr(get_called_class(), 11));
 
-        $_content = self::get_include_contents($model.'/'.$view, $vars);
-
-        if ($_content === false)
-        {
-          throw new Exception("File not found by Render: /views/$model/$view");
-        }
-
-        self::$counter--;
-
-        // Load template after all loaded views
-        // Pop on top level in stack
-        if (!self::$counter && isset(self::$template))
-        {
-            include '/views/template/'. self::$template. '.php';
-        }
+    if (!self::get_include_contents($model . '/' . $view, $vars))
+    {
+      throw new Exception("File not found by Render: /views/$model/$view");
     }
 
 
-
-    /**
-     * Return files contest in variable for example
-     * @param string $filename relative path for view
-     * @return file contest
-     */
-    protected static function get_include_contents($filename, $vars)
+    if (!--self::$counter)
     {
-        $file = realpath("views/$filename.php");
-        if (is_file($file)) {
-            ob_start();
-            foreach ($vars as $key => $val)
-            {
-                $$key = $val;
-            }
-
-            include $file;
-            $buf = ob_get_contents();
-            ob_end_flush();
-            return $buf;
-        }
-        return false;
+      $_content = ob_get_contents();
+      ob_end_clean();
+      // Load template after all loaded views
+      if (isset(static::$template))
+      {
+        $path = realpath('views/template/' . static::$template . '.php');
+        include $path;
+      }
+      else
+      {
+        echo $_content;
+      }
     }
+  }
+
+  /**
+   * Return files contest in variable for example
+   * @param string $filename relative path for view
+   * @return file contest
+   */
+  protected static function get_include_contents($filename, $vars)
+  {
+    $file = realpath("views/$filename.php");
+    if (is_file($file))
+    {
+
+      foreach ($vars as $key => $val)
+      {
+        $$key = $val;
+      }
+      include $file;
+      return true;
+    }
+    return false;
+  }
 
 }
