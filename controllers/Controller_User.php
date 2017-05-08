615503f01d43542e7bd479f5655580b3a68d7aa1
Aleksey Savelyev <saleksey92@gmail.com>
2017-05-08 23:24:44 +0600

    Add template in Controller_User.

--- controllers/Controller_User.php
+++ controllers/Controller_User.php
@@ -4,39 +4,44 @@
  * Description of Controller_User
  *
  */
-
 class Controller_User extends Controller_Base
 {
-    /**
-     * Check that exist session for user before get access in other methods
-     * @return type true - if user authorized
-     */
-    protected static function is_authorized()
+
+  protected static $template = 'main';
+
+  /**
+   * Check that exist session for user before get access in other methods
+   * @return type true - if user authorized
+   */
+  protected static function is_authorized()
+  {
+    return User::get_authorized_user() !== NULL;
+  }
+
+
+
+  public function action_index()
+  {
+    if (!self::is_authorized())
+      Router::redirect('user/login');
+    $user = User::get_authorized_user();
+    self::view('index', ['user' => $user]);
+  }
+
+
+
+  public function action_login()
+  {
+    $login = Router::post('login');
+
+    if (isset($login))
     {
-        return User::get_authorized_user() !== NULL;
+      $user = User::authorize($login, Router::post('password'));
+      if ($user->is_authorized())
+        Router::redirect('user/index');
     }
 
+    self::view('login', ['login' => $login]);
+  }
 
-
-    public function action_index()
-    {
-        if (!self::is_authorized())
-            Router::redirect('user/login');
-        $user = User::get_authorized_user();
-        self::view('index', ['user' => $user]);
-    }
-
-    public function action_login()
-    {
-        $login = Router::post('login');
-
-        if (isset($login))
-        {
-            $user = User::authorize($login, Router::post('password'));
-            if ($user->is_authorized())
-                Router::redirect('user/index');
-        }
-
-        self::view('login', ['login' => $login]);
-    }
 }
