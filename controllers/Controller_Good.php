2686c60a194753734402ce9f480ac0fce8a40f39
Aleksey Savelyev <saleksey92@gmail.com>
2017-05-10 13:34:01 +0600

    Added for Good_Review save action.

--- controllers/Controller_Good.php
+++ controllers/Controller_Good.php
@@ -46,15 +46,42 @@ class Controller_Good extends Controller_Base_Auth
 
     if (isset($values['id']))
     {
+      // UPDATE STATE
       try
       {
         $review = Good_Review::find($values['id']);
       }
       catch (Exception $ex)
       {
-
+        Session::flash('Review update is incorrect. Wrong parameters');
+        Request::redirect('/');
       }
+
+      if ($review->user_id != $user_id)
+      {
+        Session::flash('Sorry it\'s is not you review. Try later.');
+        Request::redirect('/');
+      }
+
+      unset($values['good_id']); // exclude to change existing good_id
+      $review->values($values);
     }
+    else
+    {
+      // INSERT STATE
+      $values['user_id'] = $user_id;
+      $review = new Good_Review($values);
+    }
+    
+    if ($review->save())
+    {
+      Session::flash('Review saved.');
+    }
+    else
+    {
+      Session::flash('<strong>Error:</strong> '. $review->get_error());
+    }
+    Request::redirect('/');
   }
 
 }
