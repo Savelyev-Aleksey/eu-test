2d1cbc6a103f5bbeacb365a1644fcde57829e2d0
Aleksey Savelyev <saleksey92@gmail.com>
2017-05-10 12:05:11 +0600

    DB select method refactoring: added select query to multi query exec. Added inset and update methods. Each has query preparing method. Added only exec methods and methods that returns modified rows.

--- system/DB.php
+++ system/DB.php
@@ -26,12 +26,14 @@
 
 class DB
 {
+
   /**
    * Maximum affected records on select state
    */
   const QUERY_LIM = 1000;
 
   private static $mysqli = NULL;
+  private static $error = NULL;
 
 
 
@@ -41,24 +43,27 @@ class DB
   }
 
 
+
   // Init singleton Connection to DB
   public static function init()
   {
     if (!is_null(self::$mysqli))
       return;
 
-    $settings =  parse_ini_file('settings.ini');
+    $settings = parse_ini_file('settings.ini');
 
-    self::$mysqli = new mysqli("localhost", $settings['login'],
-      $settings['password'], $settings['database']);
+    self::$mysqli = new mysqli("localhost", $settings['login'], $settings['password'], $settings['database']);
 
     /* check connection */
-    if (self::$mysqli->connect_errno) {
-        echo "Connect failed: ", self::$mysqli->connect_errno, " / ",
-          self::$mysqli->connect_error, PHP_EOL;
-        exit();
+    if (self::$mysqli->connect_errno)
+    {
+      echo "Connect failed: ", self::$mysqli->connect_errno, " / ",
+      self::$mysqli->connect_error, PHP_EOL;
+      exit();
     }
   }
+
+
 
   function __destruct()
   {
@@ -67,10 +72,12 @@ class DB
   }
 
 
+
   static function escape($value)
   {
     return self::$mysqli->real_escape_string($value);
   }
+
 
 
   // Return same array with columns instead of copying
@@ -105,9 +112,10 @@ class DB
   }
 
 
+
   private static function escape_table($table)
   {
-      return self::$mysqli->real_escape_string($table);
+    return self::$mysqli->real_escape_string($table);
   }
 
 
@@ -140,14 +148,13 @@ class DB
       if (is_string($val))
       {
         $val = '"' . self::escape($val) . '"';
-      }
-      elseif (!is_numeric($val))
+      } elseif (!is_numeric($val))
       {
         throw new Exception("Values in query must be only strings or numbers. ", 1);
       }
     }
     unset($val);
-    $where = 'WHERE ' .strtr($where, $values);
+    $where = 'WHERE ' . strtr($where, $values);
 
     return $where;
   }
@@ -173,9 +180,9 @@ class DB
       throw new Exception("fields must array or one string field, or *", 1);
     }
 
-    foreach($fields as &$f)
+    foreach ($fields as &$f)
     {
-      $f = '`'. self::escape($f). '`';
+      $f = '`' . self::escape($f) . '`';
     }
     unset($f);
 
@@ -196,24 +203,39 @@ class DB
     foreach ($fields as $f)
     {
       $fo = explode('.', $f);
-      $name = '"'. self::escape(trim($fo[0])). '"';
+      $name = '"' . self::escape(trim($fo[0])) . '"';
       $ord = self::escape(trim($fo[1]));
-      $params[] = $name. ' '. $ord;
+      $params[] = $name . ' ' . $ord;
     }
 
-    return 'ORDER BY '. implode(',', $params);
+    return 'ORDER BY ' . implode(',', $params);
   }
 
 
 
-  /*
-    get database select by mysqli and return result
-    condition must set as array('filed={val-key}', array('{val-key}' => $value))
-    set only unique key in query - see doc strtr() more info.
-  */
-  static function select($table, $condition = NULL, $fields = '*', $order = NULL, $limit = NULL)
+  /**
+   * create database select query but not execute;
+   * Added for multi query operations.
+   * For example insert new record with default values and return from DB set values.
+   *
+   * condition must set as array('filed={val-key}', array('{val-key}' => $value))
+   * set only unique key in query - see doc strtr() more info.
+   * @param string $table name where query affected
+   * @param array $condition type of ['id={id} AND ...', ['{id}' => 2, ...]]
+   * @param string or array $fields fields filter
+   * @param string $order type of 'name.ASC, date.DESC'
+   * @param type $limit
+   * @return type
+   * @throws Exception if query exec fails
+   */
+  static protected function select_query(string $table, array $condition = NULL, $fields = '*', string $order = NULL, int $limit = NULL)
   {
     self::init();
+
+    if (!strlen($table))
+    {
+      throw new Exception("Not set table to select");
+    }
 
     // 1) Safe table
     $table = self::escape_table($table);
@@ -236,14 +258,192 @@ class DB
       $limit = self::QUERY_LIM;
     }
 
-    $limit = 'LIMIT '. $limit;
+    $limit = 'LIMIT ' . $limit;
 
-    $query = "SELECT $fields FROM $table $where $order $limit";
+    return "SELECT $fields FROM $table $where $order $limit;";
+  }
+
+
+
+  /**
+   * get database select by mysqli and return result
+   * condition must set as array('filed={val-key}', array('{val-key}' => $value))
+   * set only unique key in query - see doc strtr() more info.
+   * @param string $table name where query affected
+   * @param array $condition type of ['id={id} AND ...', ['{id}' => 2, ...]]
+   * @param string or array $fields fields filter
+   * @param string $order type of 'name.ASC, date.DESC'
+   * @param type $limit
+   * @return type
+   * @throws Exception if query exec fails
+   */
+  static public function select(string $table, array $condition = NULL, $fields = '*', string $order = NULL, int $limit = NULL)
+  {
+    $query = self::select_query($table, $condition, $fields, $order, $limit);
     $res = self::$mysqli->query($query);
 
     if ($res === false)
     {
-      throw new Exception("Query was incorrect. " . $query, 1);
+      throw new Exception('Query (' . $query . ') returns error: ' . self::$mysqli->error, 1);
+    }
+
+    return $res;
+  }
+
+
+
+  /**
+   * Return prepared insert query string
+   * @param string $table
+   * @param array $raw_values associative array field_name => field_value
+   * @return string prepared insert query string
+   * @throws Exception if table or values not set
+   */
+  static protected function insert_query(string $table, array $raw_values): string
+  {
+    if (!strlen($table) || !count($raw_values))
+    {
+      throw new Exception("Nothing to insert");
+    }
+    $keys = [];
+    $values = [];
+
+    foreach ($raw_values as $key => $value)
+    {
+      $keys[] = '`' . self::escape($key) . '`';
+      $values[] = is_null($value) ? 'NULL' : '\'' . self::escape($value) . '\'';
+    }
+
+    $keys = implode(',', $keys);
+    $values = implode(',', $values);
+
+    return "INSERT INTO $table($keys) VALUES($values);";
+  }
+
+
+
+  /**
+   * Insert in DB new records only.
+   * To return newly created record use DB::insert() instead.
+   * @param string $table
+   * @param array $raw_values
+   * @return mixed MySQLi query result Object - selected from DB data
+   * @throws Exception if query exec fails
+   */
+  static public function insert_exec(string $table, array $raw_values): bool
+  {
+    $query = self::insert_query($table, $raw_values);
+    $res = self::$mysqli->query($query);
+
+    if ($res === false)
+    {
+      throw new Exception('Query (' . $query . ') returns error: ' . self::$mysqli->error, 1);
+    }
+
+    return $res;
+  }
+
+
+
+  /**
+   * Insert in DB new record and returns newly created record with filled params.
+   * For example returns set id, or default value for field.
+   * @param string $table
+   * @param array $raw_values
+   * @return mixed MySQLi query result Object - selected from DB data
+   * @throws Exception if query exec fails
+   */
+  static public function insert(string $table, array $raw_values)
+  {
+    $query = self::insert_query($table, $raw_values)
+            . "SELECT * FROM $table WHERE id = LAST_INSERT_ID();";
+    $res = self::$mysqli->query($query);
+
+    if ($res === false)
+    {
+      throw new Exception('Query (' . $query . ') returns error: ' . self::$mysqli->error, 1);
+    }
+
+    return $res;
+  }
+
+
+
+  /**
+   * Prepare query string for update query
+   * @param string $table
+   * @param array $raw_values values to be updated in record
+   * @param array $condition - WHERE condition look syntax on select_query
+   * @return string compiled query
+   * @throws Exception if table name not set
+   */
+  static protected function update_query(string $table, array $raw_values, array $condition): string
+  {
+    if (!strlen($table) || !count($raw_values))
+    {
+      throw new Exception("Nothing to update");
+    }
+
+    $where = self::escape_condition($condition);
+
+
+    $values = [];
+
+    foreach ($raw_values as $key => $value)
+    {
+      $key = '`' . self::escape($key) . '`';
+      $value = is_null($value) ? 'DEFAULT' : '\'' . self::escape($value) . '\'';
+      $values[] = $key . ' = ' . $value;
+    }
+
+    $values = implode(',', $values);
+
+    return "UPDATE $table($keys) SET $values $where;";
+  }
+
+
+
+  /**
+   * Update record and return updated records via MySQL query result
+   * @param string $table
+   * @param array $raw_values values to be updated in record
+   * @param array $condition - WHERE condition look syntax on select_query
+   * @return bool true if success
+   * @throws Exception
+   */
+  static public function update(string $table, array $raw_values, array $condition)
+  {
+    $query = self::update_query($table, $raw_values, $condition)
+            . "SELECT * FROM $table WHERE id = LAST_INSERT_ID();";
+
+    $res = self::$mysqli->query($query);
+
+    if ($res === false)
+    {
+      throw new Exception('Query (' . $query . ') returns error: ' . self::$mysqli->error, 1);
+    }
+
+    return $res;
+  }
+
+
+
+  /**
+   * Update record without return updated records
+   * @param string $table
+   * @param array $raw_values values to be updated in record
+   * @param array $condition - WHERE condition look syntax on select_query
+   * @return bool true if success
+   * @throws Exception
+   */
+  static public function update_exec(string $table, array $raw_values, array $condition): bool
+  {
+    $query = self::update_query($table, $raw_values, $condition);
+    $res = self::$mysqli->query($query);
+
+    if ($res === false)
+    {
+      throw new Exception('Query (' . $query . ') returns error: ' . self::$mysqli->error, 1);
     }
 
     return $res;
